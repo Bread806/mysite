a10e8be1f27d98a73f93054719a41735d1239019
Bread806 <bsw0210903@gmail.com>
2024-10-14 18:21:47 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+    <input v-model="message" placeholder="Edit me">
+    <p>Character count: {{ message.length }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+input {
+  padding: 10px;
+  font-size: 16px;
+  margin: 10px;
+}
+
+h1, p {
+  margin-bottom: 20px;
+}
+</style>
